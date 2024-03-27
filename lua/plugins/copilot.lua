5828dcbda7e792f15cb448609b5353a7dd47a96b
Walber Vaz <wvs.walber@gmail.com>
2024-03-27 17:49:14 -0300

    feat: update plugin copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = { enabled = true },
+      panel = { enabled = true },
+    },
+  },
+}
